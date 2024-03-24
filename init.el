e0971c309df20b18c272d7b6a8e8f593ebf06ef8
Danilo Eduardo <Danilo>
2024-03-24 14:49:48 -0300

    Begin to use use-package

--- init.el
+++ init.el
@@ -3,6 +3,24 @@
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 
+;; Packages
+(use-package company
+  :ensure t
+  :config
+  (add-hook 'after-init-hook 'global-company-mode))
+
+(use-package magit
+  :ensure t)
+
+(use-package org
+  :ensure t
+  :config
+  (require 'org)
+  (custom-set-default 'org-directory "~/org/")
+  (custom-set-default 'org-agenda-files (list org-directory))
+  (add-to-list 'org-modules 'org-habit t)
+  (setq org-src-fontify-natively t))
+
 ;; Don't show the splash screen
 (setq inhibit-startup-message t)
 
@@ -20,35 +38,6 @@
 ;; Change (yes or no) prompt to (y or n)
 (setq use-short-answers t)
 
-;; Use Company Mode (show drop down list of sugestions) in all buffers
-(add-hook 'after-init-hook 'global-company-mode)
-
 ;; Enable IDO (Interactively DO things) everywhere to show completions in minibuffer
 (ido-mode 1)
 (setq ido-enable-flex-matching t)
-
-;; Configure Org Mode
-(require 'org)
-(custom-set-default 'org-directory "~/org/")
-(custom-set-default 'org-agenda-files (list org-directory))
-(add-to-list 'org-modules 'org-habit t)
-(setq org-src-fontify-natively t) ;; syntax highlight code blocks
-
-;; Set tab to 4 spaces
-(setq-default indent-tabs-mode nil)
-(setq-default tab-width 2)
-(setq indent-line-function 'insert-tab)
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(org-babel-load-languages '((emacs-lisp . t) (python . t)))
- '(package-selected-packages '(lua-mode pyvenv htmlize org eglot magit company)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
