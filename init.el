b44cead7ec0c89c97420c009224de4087cf432bd
Danilo Eduardo <Danilo>
2024-03-26 20:09:15 -0300

    add org-download and electric parentheses

--- init.el
+++ init.el
@@ -15,11 +15,31 @@
 (use-package org
   :ensure t
   :config
-  (require 'org)
   (custom-set-default 'org-directory "~/org/")
   (custom-set-default 'org-agenda-files (list org-directory))
   (add-to-list 'org-modules 'org-habit t)
-  (setq org-src-fontify-natively t))
+  (setq org-src-fontify-natively t)
+  (setq org-list-allow-alphabetical t)
+  (org-babel-do-load-languages
+   'org-babel-load-languages
+   '((python . t))))
+
+(use-package org-download
+  :ensure t
+  :config
+  (setq-default org-download-method 'directory)
+  (setq-default org-download-image-dir "./images"))
+
+(use-package eglot
+  :ensure t)
+
+(use-package pyvenv
+  :ensure t)
+
+(use-package ox-hugo
+  :ensure t   ;Auto-install the package from Melpa
+  :pin melpa  ;`package-archives' should already have ("melpa" . "https://melpa.org/packages/")
+  :after ox)
 
 ;; Don't show the splash screen
 (setq inhibit-startup-message t)
@@ -38,6 +58,22 @@
 ;; Change (yes or no) prompt to (y or n)
 (setq use-short-answers t)
 
+;; Automatically close parentheses
+(electric-pair-mode 1)
+
 ;; Enable IDO (Interactively DO things) everywhere to show completions in minibuffer
 (ido-mode 1)
 (setq ido-enable-flex-matching t)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(org-download ox-hugo pyvenv magit company)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
